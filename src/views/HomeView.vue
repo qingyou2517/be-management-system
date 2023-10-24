e03a7cc8f9f434cc28dcc06adf4751b84db5e040
qingyou2517 <2517789608@qq.com>
2023-10-24 18:36:44 +0800

    feat: 按需导入element-plus

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="home">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+module.exports = {
+  configureWebpack: (config) => {
+    // 把配置项往 config.plugins 数组里push
+    config.plugins.push(
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      })
+    ),
+      config.plugins.push(
+        Components({
+          resolvers: [ElementPlusResolver()]
+        })
+      )
+  }
+}
